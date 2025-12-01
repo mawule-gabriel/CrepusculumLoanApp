5e1ffa6f8c8df24395db6355ea78b20ba8146d98
mawule-gabriel <mawulegabriel@gmail.com>
2025-12-01 18:07:41 +0000

    chore(config): enable Spring Data web support with DTO page serialization

---
+++ src/main/java/com/crepusculum/loanapp/travel_loan_manager/config/WebConfig.java
@@ -0,0 +1,9 @@
+package com.crepusculum.loanapp.travel_loan_manager.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+@Configuration
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
+public class WebConfig {
+}
