5a293ce9a3893720b7f49330c3175151c1a3013b
mawule-gabriel <mawulegabriel@gmail.com>
2025-12-01 11:26:38 +0000

    feat(exception): add ImageUploadException and InvalidImageException for image handling errors

---
+++ src/main/java/com/crepusculum/loanapp/travel_loan_manager/exception/InvalidImageException.java
@@ -0,0 +1,7 @@
+package com.crepusculum.loanapp.travel_loan_manager.exception;
+
+public class InvalidImageException extends RuntimeException {
+    public InvalidImageException(String message) {
+        super(message);
+    }
+}
