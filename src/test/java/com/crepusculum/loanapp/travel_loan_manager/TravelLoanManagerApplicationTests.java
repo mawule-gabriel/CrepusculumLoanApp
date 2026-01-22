89d54d6b7ce65ac3661d927df52799034573bbfd
mawule-gabriel <mawulegabriel@gmail.com>
2026-01-22 09:43:14 +0000

    test: add context load test with test mail configuration

--- src/test/java/com/crepusculum/loanapp/travel_loan_manager/TravelLoanManagerApplicationTests.java
+++ src/test/java/com/crepusculum/loanapp/travel_loan_manager/TravelLoanManagerApplicationTests.java
@@ -1,9 +1,12 @@
 package com.crepusculum.loanapp.travel_loan_manager;
 
+import com.crepusculum.loanapp.travel_loan_manager.config.TestMailConfiguration;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@Import(TestMailConfiguration.class)
 class TravelLoanManagerApplicationTests {
 
 	@Test
